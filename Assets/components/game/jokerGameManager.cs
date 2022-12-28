5d602bccca93b2064e65cce595f80a58fdbb5e75
noiz <shrinjoybiswas0@gmail.com>
2022-12-28 22:11:22 +0530

    update

--- Assets/components/game/jokerGameManager.cs
+++ Assets/components/game/jokerGameManager.cs
@@ -1,7 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Drawing;
-using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class jokerGameManager : timeManager                     
