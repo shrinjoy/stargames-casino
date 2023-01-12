ba76d480940db5a766ee07c2255584c91f743812
noiz <shrinjoybiswas0@gmail.com>
2023-01-12 14:41:52 +0530

    update

--- Assets/components/game/UI/joker_result_info.cs
+++ Assets/components/game/UI/joker_result_info.cs
@@ -7,6 +7,7 @@ using System.Data.SqlClient;
 using System;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using System.Globalization;
 
 public class joker_result_info : MonoBehaviour
 {
@@ -70,8 +71,7 @@ public class joker_result_info : MonoBehaviour
         } 
         if(mode == 1)
         {
-            sqlCmnd.CommandText = "(SELECT TOP (100)[star].[dbo].[tasp].bar,[star].[dbo].[tasp].g_id,[star].[dbo].[tasp].tot,[star].[dbo].[tasp].status,[star].[dbo].[tasp].g_time,[star].[dbo].[tasp].p_time,[star].[dbo].[resultsTaa].result as gameresult FROM [star].[dbo].[tasp] ,[star].[dbo].[resultsTaa] where  ter_id=" + GameObject.FindObjectOfType<userManager>().getUserData().id+")";//this is the sql command we use to get data about user
-
+            sqlCmnd.CommandText = "SELECT [star].[dbo].[tasp].bar,[star].[dbo].[tasp].g_id,[star].[dbo].[tasp].tot,[star].[dbo].[tasp].status,\r\n[star].[dbo].[tasp].g_time,[star].[dbo].[tasp].p_time,[star].[dbo].[resultsTaa].result as gameresult\r\nFROM [star].[dbo].[tasp],[star].[dbo].[resultsTaa]\r\nWHERE resultsTaa.g_date=tasp.g_date and resultsTaa.g_time=tasp.g_time and ter_id=" + GameObject.FindObjectOfType<userManager>().getUserData().id;
         }
         sqlData = sqlCmnd.ExecuteReader(CommandBehavior.SingleResult);
     
@@ -88,7 +88,7 @@ public class joker_result_info : MonoBehaviour
 
                 GameObject gb = (GameObject)Instantiate(historyprefab, historycontent.transform.position, Quaternion.identity, historycontent.transform);
                 print("making history");
-                gb.GetComponent<jokerhistoryobject>().setData(sqlData["bar"].ToString().Substring(6,15), sqlData["g_id"].ToString(), sqlData["tot"].ToString(), sqlData["tot"].ToString(), sqlData["status"].ToString(), "null", sqlData["g_time"].ToString(), sqlData["p_time"].ToString());
+                gb.GetComponent<jokerhistoryobject>().setData(sqlData["bar"].ToString().Substring(30).ToString(), sqlData["g_id"].ToString(), sqlData["tot"].ToString(), sqlData["tot"].ToString(), sqlData["status"].ToString(), "null", sqlData["g_time"].ToString(),sqlData["p_time"].ToString());
                
             }
 
---
+++ Assets/components/game/UI/report_info_data.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using UnityEngine;
+
+public class report_info_data : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] TMPro.TMP_Text from_time;
+    [SerializeField] TMPro.TMP_Text to_time;
+    [SerializeField] TMPro.TMP_Text salepoint;
+    [SerializeField] TMPro.TMP_Text winpoint;
+    [SerializeField] TMPro.TMP_Text commipoint;
+    [SerializeField] TMPro.TMP_Text ntppoint;
+    [SerializeField] TMPro.TMP_Text operatorpoint;
+    [SerializeField] calender tocalender;
+    [SerializeField] calender fromcalender;
+    public void setdata()
+    {
+        SqlCommand sqlCmnd = new SqlCommand();
+        SqlDataReader sqlData = null;
+        sqlCmnd.CommandTimeout = 60;
+        sqlCmnd.Connection = GameObject.FindObjectOfType<SQL_manager>().SQLconn;
+        sqlCmnd.CommandType = CommandType.Text;
+        sqlCmnd.CommandText = "select distinct g.term_name,n.ter_id plyid,ISNULL(sum(qty),0) as ppoint,ISNULL(sum(clm),0) as wpoint,\r\nISNULL(sum(qty),0)-ISNULL(sum(clm),0) as epoint,ISNULL(sum(qty),0)-ISNULL(sum(clm),0)-(ISNULL(sum(qty),0)*g.comm/100) as npoint,\r\nISNULL(sum(qty),0)*g.comm/100 as ppoints from tasp n, g_master g \r\nwhere g.term_id=n.ter_id and n.id is not null and n.status not in('Canceled') and g_date between '"+fromcalender.date+"' and  '"+tocalender.date+"'\r\ngroup by n.ter_id,g.term_name,g.comm";//this is the sql command we use to get data about user
+        sqlData = sqlCmnd.ExecuteReader(CommandBehavior.SingleResult);
+        if(sqlData.Read())
+        {
+            from_time.text = fromcalender.date;
+            to_time.text=tocalender.date;
+            salepoint.text = sqlData["ppoint"].ToString();
+            winpoint.text = sqlData["wpoint"].ToString();
+            commipoint.text = sqlData["epoint"].ToString();
+            ntppoint.text = sqlData["npoint"].ToString();
+            operatorpoint.text = sqlData["ppoint"].ToString();
+            print(sqlData["plyid"].ToString());
+        }
+        sqlData.Close();
+    }
+}
