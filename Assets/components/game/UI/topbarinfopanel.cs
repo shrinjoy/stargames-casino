e91c8f895aaa98a558b6a9ae4fb7136dca474cdd
noiz <shrinjoybiswas0@gmail.com>
2023-01-13 20:29:16 +0530

    update

--- Assets/components/game/UI/topbarinfopanel.cs
+++ Assets/components/game/UI/topbarinfopanel.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class topbarinfopanel : MonoBehaviour
 {
     [SerializeField]public TMPro.TMP_Text balancetext;
+    [SerializeField] public TMPro.TMP_Text fakebalance;
     [SerializeField] TMPro.TMP_Text userid;
     [SerializeField] TMPro.TMP_Text gameid;
     // Start is called before the first frame update
@@ -17,6 +18,7 @@ public class topbarinfopanel : MonoBehaviour
     {
         int id = Convert.ToInt32(GameObject.FindObjectOfType<userManager>().getUserData().id);
         balancetext.text = GameObject.FindObjectOfType<SQL_manager>().balance(id).ToString();
+      
   
     }
     // Update is called once per frame
@@ -24,5 +26,7 @@ public class topbarinfopanel : MonoBehaviour
     {
         userid.text = GameObject.FindObjectOfType<userManager>().getUserData().id;
         gameid.text = GameObject.FindObjectOfType<betManager>().gameResultId.ToString();
+        fakebalance.text = Math.Clamp((Convert.ToInt32(balancetext.text) - Convert.ToInt32(GameObject.FindObjectOfType<totalbet>().totalbetamount)), 0, 99999999).ToString();
+
     }
 }
